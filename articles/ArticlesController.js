2dacb256644ad26c292cb0fcd6ee35450dcd4b54
Tiago Soares <tiago-s@outlook.com>
2020-03-23 03:37:37 -0300

    add timymce editor and create view add new article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const Router = express.Router()
+const Category = require('../categories/Category')
 
-Router.get('/articles', (req, res)=>{
-    res.send('Articles')
+const pathURL = '/admin/articles'
+
+const pathViews = 'admin/articles/'
+
+
+Router.get(pathURL + '/new', (req, res)=>{
+
+	Category.findAll().then( categories => {
+		res.render(pathViews + '/new', {categories: categories})
+	})
+
 })
 
 
